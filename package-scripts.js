6d3a8984b5b972f5601d66ea098761196efe8119
Outsider <outsideris@gmail.com>
2021-04-02 21:02:07 +0900

    ci: lint code with eslint
    Signed-off-by: Outsider <outsideris@gmail.com>

--- package-scripts.js
+++ package-scripts.js
@@ -1,27 +1,37 @@
 'use strict';
 
 function test(testName, mochaParams) {
-  const mochaCommand = 'mocha'
+  const mochaCommand = 'mocha';
   return `${mochaCommand} ${mochaParams}`.trim();
 }
 
 module.exports = {
   scripts: {
+    lint: {
+      default: {
+        script: 'nps lint.code',
+        description: 'Lint code',
+      },
+      code: {
+        script: 'eslint . "bin/*"',
+        description: 'Runt ESLint linter',
+      },
+    },
     test: {
       default: {
         script: 'nps test.node',
-        description: 'Run all tests'
+        description: 'Run all tests',
       },
       node: {
         default: {
           script: 'nps test.node.unit',
-          description: 'Run Node.js tests'
+          description: 'Run Node.js tests',
         },
         unit: {
           script: test('unit', '"test/node-unit/**/*.spec.js"'),
-          description: 'Run Node.js unit tests'
-        }
-      }
-    }
-  }
+          description: 'Run Node.js unit tests',
+        },
+      },
+    },
+  },
 };
